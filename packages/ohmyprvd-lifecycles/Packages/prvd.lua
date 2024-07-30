21e3da93c44892f4d41965c88aadf88e88cc440d
znotfireman <znotfireman@gmail.com>
2024-07-30 16:47:59 +0700

    chore: drop files from .gitignore

--- packages/ohmyprvd-lifecycles/Packages/prvd.lua
+++
@@ -1,4 +0,0 @@
--- too lazy to use wally package types
-local REQUIRED_MODULE = require(script.Parent._Index["znotfireman_ohmyprvd@0.1.1-alpha"]["ohmyprvd"])
-export type Provider<T> = REQUIRED_MODULE.Provider<T>
-return REQUIRED_MODULE
