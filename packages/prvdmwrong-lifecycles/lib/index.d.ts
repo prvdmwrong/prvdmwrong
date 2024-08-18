d0e70f5ab312cca810d0fcf6b05b7d8941ef58a5
znotfireman <znotfireman@gmail.com>
2024-08-18 13:03:27 +0700

    feat: update ts types

--- packages/prvdmwrong-lifecycles/lib/index.d.ts
+++ packages/prvdmwrong-lifecycles/lib/index.d.ts
@@ -20,80 +20,94 @@
  * SOFTWARE.
  */
 
-/**
- * Hook onto the onPreSimulation lifecycle method. Equivalent to the
- * RunService.PreSimulation event.
- */
-export interface OnPreSimulation {
-	/**
-	 * Runs every frame before physic simulation.
-	 */
-	onPreSimulation(dt: number): void
-}
+import { type Lifecycle } from "@prvdmwrong/core"
 
-/**
- * Hook onto the onPreSimulation lifecycle method. Equivalent to the
- * RunService.PostSimulation event.
- */
-export interface OnPostSimulation {
+export = Lifecycles
+export as namespace Lifecycles
+declare namespace Lifecycles {
 	/**
-	 * Runs every frame after physic simulation.
+	 * Hook onto the onPreSimulation lifecycle method. Equivalent to the
+	 * RunService.PreSimulation event.
 	 */
-	onPostSimulation(dt: number): void
-}
+	export interface OnPreSimulation {
+		/**
+		 * Runs every frame before physic simulation.
+		 */
+		onPreSimulation(dt: number): void
+	}
 
-/**
- * Hook onto the onPreAnimation lifecycle method. Equivalent to the
- * RunService.PreAnimation event.
- */
-export interface OnPreAnimation {
 	/**
-	 * Runs every prior to the physics simulation but after rendering. Only runs
-	 * on the client.
+	 * Hook onto the onPreSimulation lifecycle method. Equivalent to the
+	 * RunService.PostSimulation event.
 	 */
-	onPreAnimation(dt: number): void
-}
+	export interface OnPostSimulation {
+		/**
+		 * Runs every frame after physic simulation.
+		 */
+		onPostSimulation(dt: number): void
+	}
 
-/**
- * Hook onto the onPreRender lifecycle method. Equivalent to the
- * RunService.PreRender event.
- */
-export interface OnPreRender {
 	/**
-	 * Runs every frame before rendering. Only runs on the client.
+	 * Hook onto the onPreAnimation lifecycle method. Equivalent to the
+	 * RunService.PreAnimation event.
 	 */
-	onPreRender(dt: number): void
-}
+	export interface OnPreAnimation {
+		/**
+		 * Runs every prior to the physics simulation but after rendering. Only runs
+		 * on the client.
+		 */
+		onPreAnimation(dt: number): void
+	}
 
-/**
- * Hook onto the onShutdown lifecycle method. Equivalent to game:BindToClose or
- * the plugin.Unloading event for plugins.
- */
-export interface OnShutdown {
 	/**
-	 * Runs just before the game or plugin shuts down.
+	 * Hook onto the onPreRender lifecycle method. Equivalent to the
+	 * RunService.PreRender event.
 	 */
-	onShutdown(): void
-}
+	export interface OnPreRender {
+		/**
+		 * Runs every frame before rendering. Only runs on the client.
+		 */
+		onPreRender(dt: number): void
+	}
 
-/**
- * Hook onto the onPlayerAdded lifecycle method. Equivalent to the
- * Players.PlayerAdded event.
- */
-export type OnPlayerAdded = {
 	/**
-	 * Runs when a player joined and for every existing player prior to startup.
+	 * Hook onto the onShutdown lifecycle method. Equivalent to game:BindToClose or
+	 * the plugin.Unloading event for plugins.
 	 */
-	onPlayerAdded(player: Player): void
-}
+	export interface OnShutdown {
+		/**
+		 * Runs just before the game or plugin shuts down.
+		 */
+		onShutdown(): void
+	}
 
-/**
- * Hook onto the onPlayerRemoving lifecycle method. Equivalent to the
- * Players.PlayerRemoved event.
- */
-export type OnPlayerRemoving = {
 	/**
-	 * Runs just before a player leaves the game.
+	 * Hook onto the onPlayerAdded lifecycle method. Equivalent to the
+	 * Players.PlayerAdded event.
 	 */
-	onPlayerRemoving(player: Player): void
+	export type OnPlayerAdded = {
+		/**
+		 * Runs when a player joined and for every existing player prior to startup.
+		 */
+		onPlayerAdded(player: Player): void
+	}
+
+	/**
+	 * Hook onto the onPlayerRemoving lifecycle method. Equivalent to the
+	 * Players.PlayerRemoved event.
+	 */
+	export type OnPlayerRemoving = {
+		/**
+		 * Runs just before a player leaves the game.
+		 */
+		onPlayerRemoving(player: Player): void
+	}
+
+	export const preSimulation: Lifecycle<OnPreSimulation>
+	export const postSimulation: Lifecycle<OnPostSimulation>
+	export const preAnimation: Lifecycle<OnPreAnimation>
+	export const preRender: Lifecycle<OnPreRender>
+	export const shutdown: Lifecycle<OnShutdown>
+	export const playerAdded: Lifecycle<OnPlayerAdded>
+	export const playerRemoving: Lifecycle<OnPlayerRemoving>
 }
